dda9cc7ba2688cca3e9530cc16fe448a96cce56f
MahammadNiyazli <mahammad.niyazli@gmail.com>
2023-07-14 15:52:06 +0400

    first commit

---
+++ src/main/java/com/company/msingressfeign/MsIngressFeignApplication.java
@@ -0,0 +1,13 @@
+package com.company.msingressfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsIngressFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsIngressFeignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/msingressfeign/MsIngressFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.msingressfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsIngressFeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
